7048c5806ec24e19593212c0ca0d21a39779515a
Ashraf Hashimy <49528326+1Ashraf2@users.noreply.github.com>
2024-08-11 03:24:31 +0100

    Update provider.tf

--- dev/services/jenkins-slave/provider.tf
+++ dev/services/jenkins-slave/provider.tf
@@ -1,1 +1,19 @@
+terraform {
+  required_version = ">= 0.13.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.49.0"
+    }
+  }
+}
 
+terraform {
+  backend "s3" {
+    bucket         = "ash-ter-cert-bucket-2024"
+    key            = "dev/jenkins-agent/terraform.tfstate"
+    region         = "eu-west-1"
+    encrypt        = true
+    dynamodb_table = "Terraform-statefile"
+  }
+}
