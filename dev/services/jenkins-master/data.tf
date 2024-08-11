01840a95034b5531845c3b74b0fba7b73bbf6f3a
Ashraf Hashimy <49528326+1Ashraf2@users.noreply.github.com>
2024-08-11 03:29:02 +0100

    Update data.tf

--- dev/services/jenkins-master/data.tf
+++ dev/services/jenkins-master/data.tf
@@ -1,1 +1,13 @@
+data "aws_vpc" "default" {
+  default = true
+}
+
+
+#Data sources to lookup existing subnets by tag
+data "aws_subnet" "public" {
+  filter {
+    name   = "tag:Name"
+    values = ["dev-public-2"]
+  }
+}
 
